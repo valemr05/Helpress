17020e316e926ce7fe47aac7fde47c6ce94440d0
valemr05 <valeria.munoz3@utp.edu.co>
2025-08-05 16:45:40 -0500

    serviceComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ServiceFormComponent} from './service-form/service-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ServiceFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Helpress';
+  title = 'Helpress Application';
 }
