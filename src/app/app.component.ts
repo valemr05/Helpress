2eaf8a32d7c8f08601e11baf5e02bd711bb34807
valemr05 <valeria.munoz3@utp.edu.co>
2025-08-05 17:59:56 -0500

    style services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {ServiceFormComponent} from './service-form/service-form.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ServiceFormComponent],
+  imports: [ServiceFormComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/service-form/service-form.component.ts
+++ src/app/service-form/service-form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-service-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './service-form.component.html',
   styleUrl: './service-form.component.css'
 })
-export class ServiceFormComponent {
 
+export class ServiceFormComponent {
+  categorias = ['A/A', 'Plomería', 'Electricidad', 'Jardinería'];
+  subcategorias = ['Instalación', 'Reparación', 'Mantenimiento'];
+
+  form = {
+    categoria: '',
+    subcategoria: '',
+    titulo: '',
+    descripcion: '',
+    ubicacion: '',
+    foto: null as File | null
+  };
+
+  subirFoto(event: any) {
+    this.form.foto = event.target.files[0];
+  }
+
+  enviarFormulario() {
+    console.log('Datos del formulario:', this.form);
+    
+  }
 }
